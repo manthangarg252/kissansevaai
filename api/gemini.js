d73231106292570b3a7fa34c2ca995a73e329f1f
Manthan Garg <gargmanthan525@gmail.com>
2026-01-20 19:34:23 +0530

    apiii

--- api/gemini.js
+++ api/gemini.js
@@ -23,9 +23,10 @@ export default async function handler(req, res) {
       return res.status(500).json({ error: "Missing GEMINI_API_KEY in server env" });
     }
 
-    const url =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" +
-      apiKey;
+   const url =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
+  apiKey;
+
 
     const parts = [];
 
